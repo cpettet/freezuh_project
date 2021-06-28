84a30c7dc0498d6a5dbe07e3ad98fa87b6b00e66
cpettet <cpettet11@gmail.com>
2021-06-28 14:39:44 -0700

    Backend initializing. Seed commands finished. Registered and added all relevant packages. About to start on user authorization/authentication.

---
+++ application/__init__.py
@@ -0,0 +1,61 @@
+import os
+from flask import Flask, request, session, redirect
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect, generate_csrf
+from flask_login import LoginManager
+
+from .models import db, User
+from .seeds import seed_commands
+from .config import Config
+# Space for importing routes from blueprints
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.cli.add_command(seed_commands)
+# Space for registering routes from blueprints
+
+db.init_app(app)
+Migrate(app, db)
+
+# Setup login manager
+login = LoginManager(app)
+login.login_view = "auth.unauthorized"
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+# application security
+CORS(app)
+
+
+# Redirect to https when using http requests
+@app.before_request
+def https_redirect():
+    if os.environ.get('FLASK_ENV') == 'production':
+        if request.headers.get('X-Forwarded-Proto') == 'http':
+            url = request.url.replace('http://', 'https://', 1)
+            code = 301
+            return redirect(url, code=code)
+
+
+@app.after_request
+def inject_csrf_token(response):
+    response.set_cookie('csrf_token',
+                        generate_csrf(),
+                        secure=True if os.environ.get(
+                            'FLASK_ENV') == 'production' else False,
+                        samesite='Strict' if os.environ.get(
+                            'FLASK_ENV') == 'production' else None,
+                        httponly=True)
+    return response
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def react_root(path):
+    print("path", path)
+    if path == 'favicon.ico':
+        return app.send_static_file('favicon.ico')
+    return app.send_static_file('index.html')
---
+++ application/models/user.py
@@ -0,0 +1,10 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    
