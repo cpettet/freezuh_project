fce6a6a6db81a8190074d26fadac8c9330e330ab
cpettet <cpettet11@gmail.com>
2021-06-29 15:04:22 -0700

    At a roadblock for the SampleForm validation issue. Created thunks for my redux store. Created a standard state; renders correctly in the redux debugger.

--- application/forms/sample_form.py
+++ application/forms/sample_form.py
@@ -1,16 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField
-from wtforms.fields.core import StringField, IntegerField, DateTimeField
-from wtforms.validators import DataRequired
-from application.models import Sample
+from wtforms.fields.core import (BooleanField, StringField, IntegerField,
+                                 DateTimeField)
+from wtforms.validators import DataRequired, ValidationError
+from application.models.sample import SampleType
 
 
-def sample_type_exists(field):
-    pass
+def sample_type_exists(form, field):
+    """
+    Checks if the given sample_type is in the list of valid sample_types 
+    presented in the model.
+    """
+    sample_type = field.data
+    valid_sample_types = [enumerated.value for enumerated in list(SampleType)]
+    if sample_type not in valid_sample_types:
+        raise ValidationError("Sample type is invalid.")
 
 
 class SampleForm(FlaskForm):
     plate_id = IntegerField("plate_id")
     box_id = IntegerField("box_id")
-    sample_type = StringField("sample_type", validators=[DataRequired()])
+    sample_type = StringField("sample_type", validators=[
+                              DataRequired(), sample_type_exists])
+    accession_date = DateTimeField("accession_date")
+    store_date = DateTimeField("store_date")
     thaw_count = IntegerField("thaw_count")
+    expiry_date = DateTimeField("expiry_date")
+    discarded = BooleanField("discarded")
---
+++ frieza-app/src/store/sample.js
@@ -0,0 +1,105 @@
+// constants
+const GET_SAMPLE = "sample/GET_POST";
+const POST_SAMPLE = "sample/POST_SAMPLE";
+const EDIT_SAMPLE = "sample/EDIT_SAMPLE";
+const DELETE_SAMPLE = "sample/DELETE_SAMPLE";
+
+// action creators
+const get = (samples) => ({
+  type: GET_SAMPLE,
+  samples,
+});
+
+const post = sample => ({
+  type: POST_SAMPLE,
+  sample,
+});
+
+const edit = sample => ({
+  type: EDIT_SAMPLE,
+  sample,
+});
+
+const deleteAction = sample => ({
+  type: DELETE_SAMPLE,
+  sample,
+})
+
+// thunks
+export const getSamples = () => async dispatch => {
+  const response = await fetch("/api/samples");
+  const data = await response.json();
+
+  dispatch(get(data.samples));
+};
+
+export const createSample = data => async dispatch => {
+  const response = await fetch("/api/samples", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (response.ok) {
+    const newSample = await response.json();
+    dispatch(post(newSample));
+  };
+};
+
+export const editSample = data => async dispatch => {
+  const response = await fetch(`/api/samples${data.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (response.ok) {
+    const editedSample = await response.json();
+    dispatch(edit(editedSample))
+  }
+}
+
+export const deleteSample = data => async dispatch => {
+  // LEFT OFF HERE
+  return;
+}
+
+const initialState = {byId: {}, allIds: {},};
+// samples: {
+//   byId: {
+//     "sample1": {},
+//     "sample2": {},
+//   },
+//   allIds: ["sample1", "sample2"]
+// }
+
+// reducer
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_SAMPLE: {
+      const newState = { ...state };
+      action.samples.forEach(sample => {
+        newState.byId[sample.id] = sample;
+        newState.allIds.append(sample.id);
+      });
+      return newState;
+    }
+    case POST_SAMPLE: {
+      const newState = { ...state };
+      newState.byId[action.sample.id] = action.sample;
+      newState.allIds.append(action.sample.id);
+      return newState;
+    }
+    case EDIT_SAMPLE: {
+      const newState = { ...state };
+      newState.byId[action.sample.id] = action.sample;
+      return newState;
+    }
+    default: 
+      return state;
+  }
+}
