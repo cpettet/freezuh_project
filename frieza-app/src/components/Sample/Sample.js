06cb0359d4ddac72a74907e2f67cea36bfdda97b
cpettet <cpettet11@gmail.com>
2021-06-30 13:26:50 -0700

    Changes to dockerfile. Working Sample showing and link.

--- frieza-app/src/components/Sample/Sample.js
+++ frieza-app/src/components/Sample/Sample.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import style from "./Sample.module.css";
-import image from "./Sample-Tube.min.svg"
+import image from "./Sample-Tube.min.svg";
 
-function Sample({sample}) {
+function Sample({ sample }) {
   const dispatch = useDispatch();
 
   return (
     <div className={style.sample}>
-      <img src={image} alt={sample.id} className={style.sample__icon}/>
-      <p>{sample.id}</p>
+      <Link to={`/samples/${sample.id}`}>
+        <img src={image} alt={sample.id} className={style.sample__icon} />
+        <p>{sample.id}</p>
+      </Link>
     </div>
-  )
+  );
 }
 
 export default Sample;
