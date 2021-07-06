bcc220d71d03beada5eba869a76e8e8f7c683f3f
cpettet <cpettet11@gmail.com>
2021-07-05 18:22:59 -0700

    Added backend routes' blueprints. Added psueudocode for the store. Time for a break.

---
+++ react-app/src/store/rack.js
@@ -0,0 +1,22 @@
+// constants
+
+// action creators
+
+// thunks
+
+const initialState = {
+  byId: {},
+  allIds: [],
+};
+// racks: {
+//   byId: {
+//     "rack1": {},
+//     "rack2": {},
+//   },
+//   allIds: ["rack1", "rack2"]
+// }
+
+// reducer
+export default function reducer(state=initialState, action) {
+    return;
+}
